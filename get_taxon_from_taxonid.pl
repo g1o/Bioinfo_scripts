36eb6dcdfd1108f31aef1cf8410173de8eec6884
Giovanni Marques de Castro <innavoig@walla.co.il>
2018-11-25 01:35:13 -0200

    added a hash to increase the speed a 100 times
    now in ~1min it does 1 million lines.

--- get_taxon_from_taxonid.pl
+++ get_taxon_from_taxonid.pl
@@ -4,17 +4,17 @@ use Bio::Tree::Tree;
 use Getopt::Long;
 use warnings;
 use strict;
-my $classified_reads="/home/giovannimc/projects/Carol_metagenomics-cacao/work/noadapter/version2/KAIJU/A.combined.tsv.head";
+my $classified_reads="PATH/version2/KAIJU/A.combined.tsv.head";
 #my $tree_functions = Bio::Tree::Tree->new();
 GetOptions (#"length=i" => \$length,    # numeric
               "input=s" => \$classified_reads,      # string
 #              "verbose"  => \$verbose   # flag
 );
 
-my $binningdir="/home/giovannimc/projects/Carol_metagenomics-cacao/work/noadapter/version2/KAIJU/";
-my $datadir="/home/giovannimc/projects/Carol_metagenomics-cacao/data/cutadapter/Filtered/";
+my $binningdir="PATH/noadapter/version2/KAIJU/";
+my $datadir="PATH/data/cutadapter/Filtered/";
 open(CLASSIFIED_READS,"<$classified_reads")||die "cannot open $classified_reads\n";
-#f in /home/giovannimc/projects/Carol_metagenomics-cacao/work/noadapter/KAIJU/[^ABCD].combined.tsv
+
 # Get one from a database
 my $dbh = Bio::DB::Taxonomy->new(-source   => 'flatfile',
                                  -directory=> '/home/giovannimc/genomas_and_databases/NCBItaxonomy/BioTaxon',
@@ -22,17 +22,23 @@ my $dbh = Bio::DB::Taxonomy->new(-source   => 'flatfile',
                                  -namesfile=> '/home/giovannimc/genomas_and_databases/NCBItaxonomy/names.dmp');
 
 my $taxon = $dbh->get_taxon(-taxonid => '9606'); 
+my %hashing; #keep taxid => 'other rank' for faster search.
 while(<CLASSIFIED_READS>){
         if( $_=~/^C\t(.*)\t(.*)$/){
 my $read=$1;
 my $taxid=$2;
+        if($hashing{$taxid}){
+                print "$read\t tx:$taxid\tphylum is ",$hashing{$taxid},"\n";
+        }else{
+
         $taxon = $dbh->get_taxon(-taxonid => $taxid);
         my $tree = Bio::Tree::Tree->new(-node => $taxon);
         my $lineage = $tree->find_node(-rank => 'phylum');
         #print "tx:$taxid\t".$taxon->rank.":rank\t". $tree->number_nodes . " nodes\n";
         if($lineage){ #Lineage has phylum, or any other desired rank
         print "$read\t tx:$taxid\tphylum is ",$lineage->scientific_name,"\n";
-        }
-        }
+    }
+    }
+}
 }
 
